5b7b6a2ef071fa6a869a17980cb97c466c949f15
Eliot <saltycrane@gmail.com>
2021-01-25 10:52:22 -0800

    disable CSRF per graphene docs
    https://docs.graphene-python.org/projects/django/en/latest/tutorial-plain/#creating-graphql-and-graphiql-views

--- graphene-api/pagination_ex_api/urls.py
+++ graphene-api/pagination_ex_api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from pagination_ex_api.schema import schema
@@ -7,5 +8,7 @@ from pagination_ex_api.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", GraphQLView.as_view(graphiql=True, schema=schema)),
+    # disable CSRF per graphene docs:
+    # https://docs.graphene-python.org/projects/django/en/latest/tutorial-plain/#creating-graphql-and-graphiql-views
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
